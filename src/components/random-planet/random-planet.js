1bdf38385491c6e65106c9b85c445f2e1a7172d0
Ilya Udovichenko <rios111@yandex.ru>
2021-08-15 13:36:51 +0300

    lesson 57 api data transformation

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,29 +1,57 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 
 class RandomPlanet extends Component {
+  constructor() {
+    super();
+
+    this.swapiService = new SwapiService();
+
+    this.state = {
+      planet: {}, // Сначала сделаем планету пустым объект, чтобы наш код в render() не ругался при деструктуризации.
+    };
+
+    this.updatePlanet();
+  }
+
+  // Как всегда используем функцию-стрелку, поскольку мы будем передавать эту функцию в другую фуннкцию, и нам нужно
+  // быть осторожными со значением this. И теперь мы можем вставить этот кусочек кода в updatePlanet(). Намного легче читать
+  // такой код, правда?
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25) + 2;
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+  }
+
   render() {
+    const {
+      planet: { id, name, population, rotationPeriod, diameter },
+    } = this.state;
     return (
       <div className="random-planet jumbotron rounded">
         <img
           className="planet-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
         />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
